ce1cb0498743f5278f82e17a9e6efc4bf5715564
parkjunyoung <pjoon86@naver.com>
2016-12-21 20:23:06 +0900

    게시판 작성

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 var autoIncrement = require('mongoose-auto-increment');
 
 var db = mongoose.connection;
